a43403cdd9a3c3a07acc1993c4df865d2661ff80
Kenneth Reitz <me@kennethreitz.org>
2017-12-11 11:01:43 -0500

    test runner
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- django_heroku/core.py
+++ django_heroku/core.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import dj_database_url
+from django.test.runner import DiscoverRunner
 
 MAX_CONN_AGE = 600
 
@@ -30,7 +31,40 @@ def mkdir_p(newdir):
             os.mkdir(newdir)
 
 
-def settings(config, databases=True, staticfiles=True, allowed_hosts=True, logging=True, secret_key=True):
+class HerokuDiscoverRunner(DiscoverRunner):
+    """Test Runner for Heroku CI, which provides a database for you.
+    This requires you to set the TEST database (done for you by settings().)"""
+
+    def setup_databases(self, **kwargs):
+        if not os.environ.get('CI'):
+            raise ValueError(
+                "The CI env variable must be set to enable this functionality.  WARNING:  "
+                "This test runner will wipe all tables in the database it targets!")
+        self.keepdb = True
+        return super(HerokuDiscoverRunner, self).setup_databases(**kwargs)
+
+    def _wipe_tables(self, connection):
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                    DROP SCHEMA public CASCADE;
+                    CREATE SCHEMA public;
+                    GRANT ALL ON SCHEMA public TO postgres;
+                    GRANT ALL ON SCHEMA public TO public;
+                    COMMENT ON SCHEMA public IS 'standard public schema';
+                """
+            )
+        pass
+
+    def teardown_databases(self, old_config, **kwargs):
+        self.keepdb = True
+        for connection, old_name, destroy in old_config:
+            if destroy:
+                self._wipe_tables(connection)
+        super(HerokuDiscoverRunner, self).teardown_databases(old_config, **kwargs)
+
+
+def settings(config, databases=True, test_runner=True, staticfiles=True, allowed_hosts=True, logging=True, secret_key=True):
 
     # Database configuration.
     # TODO: support other database (e.g. TEAL, AMBER, etc, automatically.)
@@ -49,13 +83,24 @@ def settings(config, databases=True, staticfiles=True, allowed_hosts=True, loggi
                 config['DATABASES'][db_color] = dj_database_url.parse(url, conn_max_age=MAX_CONN_AGE)
 
         if 'DATABASE_URL' in os.environ:
-            logger.info('Adding $DATABASE_URL to DATABASES Django setting.')
+            logger.info('Adding $DATABASE_URL to default DATABASE Django setting.')
 
             # Configure Django for DATABASE_URL environment variable.
             config['DATABASES']['default'] = dj_database_url.config(conn_max_age=MAX_CONN_AGE)
 
+            logger.info('Adding $DATABASE_URL to TEST default DATABASE Django setting.')
+
+            # Enable test database if found in CI environment.
+            if 'CI' in os.environ:
+                config['DATABASES']['default']['TEST'] = config['DATABASES']['default']
+
         else:
             logger.info('$DATABASE_URL not found, falling back to previous settings!')
+
+    if test_runner:
+        # Enable test runner if found in CI environment.
+        if 'CI' in os.environ:
+            config['TEST_RUNNER'] = 'django_heroku.HerokuDiscoverRunner'
 
     # Staticfiles configuration.
     if staticfiles:
