322c835d2dcd8d848426a7f9df6da3bd8dbc8ba2
Osaetin Daniel <osaetindaniel@gmail.com>
2017-12-11 20:18:27 +0100

    Refactored mkdir_p function to use os.makedirs

--- django_heroku/core.py
+++ django_heroku/core.py
@@ -15,20 +15,16 @@ def mkdir_p(newdir):
         - regular file in the way, raise an exception
         - parent directory(ies) does not exist, make them as well
     """
-    if os.path.isdir(newdir):
-        pass
-    elif os.path.isfile(newdir):
-        raise OSError(
-            "a file with the same name as the desired "
-            "dir, '%s', already exists." % newdir
-        )
-    else:
-        head, tail = os.path.split(newdir)
-        if head and not os.path.isdir(head):
-            mkdir_p(head)
-
-        if tail:
-            os.mkdir(newdir)
+    try:
+        os.makedirs(newdir)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(newdir):
+            pass
+        else:
+            raise OSError (
+                "a file with the same name as the desired "
+                "dir, '%s', already exists." % newdir
+            )
 
 
 class HerokuDiscoverRunner(DiscoverRunner):
