fb4b81e943e6eae8d5d806f6a453e32962497e6f
Kenneth Reitz <me@kennethreitz.org>
2017-12-12 15:11:06 +0000

    updated tests

--- test/test_django_heroku.py
+++ test/test_django_heroku.py
@@ -7,6 +7,13 @@ import pytest
 import testproject.settings as config
 
 def test_databases():
+    os.environ['DATABASE_URL'] = 'postgres://fake:fake@fake.com/fake'
+    imp.reload(config)
+    
+    assert 'postgres' in config.DATABASES['default']['ENGINE']
+    
+def test_multi_databases():
+    os.environ
     imp.reload(config)
 
 def test_test_runner():
@@ -14,6 +21,7 @@ def test_test_runner():
     
 def test_staticfiles():
     imp.reload(config)
+    
 
 def test_allowed_hosts():
     imp.reload(config)
