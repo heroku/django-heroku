694eda5bdc85c00dfac0a27554a3a3e4c264f094
Kenneth Reitz <me@kennethreitz.org>
2017-12-12 14:59:14 +0000

    test infra

--- test/test_django_heroku.py
+++ test/test_django_heroku.py
@@ -1,7 +1,31 @@
 import sys
 import os
+import imp
 
-def test_config():
-    import testproject.settings as config
+import pytest
+
+import testproject.settings as config
+
+def test_databases():
+    imp.reload(config)
+
+def test_test_runner():
+    imp.reload(config)
+    
+def test_staticfiles():
+    imp.reload(config)
+
+def test_allowed_hosts():
+    imp.reload(config)
     
     assert config.ALLOWED_HOSTS == ['*']
+    
+    
+def test_logging():
+    imp.reload(config)
+    
+def test_secret_key():
+    os.environ['SECRET_KEY'] = 'SECRET'
+
+    imp.reload(config)
+    assert config.SECRET_KEY == 'SECRET'
